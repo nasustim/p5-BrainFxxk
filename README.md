c3cec52798d6242eb75198f4527980b2153f097c
nasustim <mitsuhibino@gmail.com>
2021-01-16 15:49:06 +0900

    repo initialize

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::Nasustim::BrainFxxk - It's new $module
+
+# SYNOPSIS
+
+    use Acme::Nasustim::BrainFxxk;
+
+# DESCRIPTION
+
+Acme::Nasustim::BrainFxxk is ...
+
+# LICENSE
+
+Copyright (C) nasustim.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+nasustim <hibino17@iamas.ac.jp>
---
+++ lib/Acme/Nasustim/BrainFxxk.pm
@@ -0,0 +1,39 @@
+package Acme::Nasustim::BrainFxxk;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::Nasustim::BrainFxxk - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::Nasustim::BrainFxxk;
+
+=head1 DESCRIPTION
+
+Acme::Nasustim::BrainFxxk is ...
+
+=head1 LICENSE
+
+Copyright (C) nasustim.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+nasustim E<lt>hibino17@iamas.ac.jpE<gt>
+
+=cut
+
